675bc1b5ba7da9a05c2c63d78e5cf8744c16a2a3
philipFS <philips@fullsail.edu>
2024-08-20 17:05:08 +0000

    discuss pilots lounge

--- guiV1/pilots_lounge_entry.py
+++ guiV1/pilots_lounge_entry.py
@@ -1,6 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
 
+# init python variables
+fName = ''
+lName = ''
+
+def submit():
+    # tkinter var to python
+    fName=fName_tk.get()
+    lName=lName_tk.get()
+    print('************')
+    print(fName, lName)
+    
+    
+
 #main root window
 window = tk.Tk()
 window.title("Pilot's Lounge")
@@ -8,8 +21,14 @@ window.title("Pilot's Lounge")
 frame = tk.Frame(window)
 frame.pack()
 
-first_name = tk.StringVar()
+# tkinter variables
+fName_tk = tk.StringVar()
+lName_tk = tk.StringVar()
 
+# function to get text box info
+# Enter button clicked
+# Send tk variables to python variables
+# firestName python version
 
 pilot_info_frame = tk.LabelFrame(frame, text = "User Info")
 pilot_info_frame.grid(row = 0, column = 0, padx = 20, pady = 20)
@@ -19,9 +38,10 @@ first_name.grid(row = 0, column = 0)
 last_name = tk.Label(pilot_info_frame, text = "Last Name: ")
 last_name.grid(row = 0, column = 1)
 
-first_name_entry = tk.Entry(pilot_info_frame)
+# Entry's
+first_name_entry = tk.Entry(pilot_info_frame, textvariable=fName_tk)
 first_name_entry.grid(row = 1, column = 0)
-last_name_entry = tk.Entry(pilot_info_frame)
+last_name_entry = tk.Entry(pilot_info_frame, textvariable=lName_tk)
 last_name_entry.grid(row = 1, column = 1)
 
 email = tk.Label(pilot_info_frame, text = " Email: ")
@@ -34,7 +54,11 @@ dob.grid(row = 2, column = 0)
 dob_entry = tk.Entry(pilot_info_frame)
 dob_entry.grid(row = 3, column = 0)
 
+# pull down different drone types
 aircraft = tk.Label(pilot_info_frame, text = "Aircraft: ")
 aircraft.grid(row = 2, column = 1)
 aircraft_combobox = ttk.Combobox(pilot_info_frame, values = [])
+
+sub_btn=tk.Button(pilot_info_frame, text = 'Submit', command = submit)
+sub_btn.grid(row=3, column=2)
 window.mainloop()
